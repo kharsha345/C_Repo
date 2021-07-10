1669af26c8db789145800beff1e0c94692f129f2
kharsha345@gmail.com <kharsha345@gmail.com>
2021-07-10 09:05:45 +0530

    (Commit-10-07-2021)

--- Pointers/return_fun_ptr1.c
+++ Pointers/return_fun_ptr1.c
@@ -4,6 +4,7 @@
 
 typedef int (*fptr)(int, int);
 typedef int (*choice2(int a))(int, int);
+typedef int (*(*choice4(char a))(int))(int, int);
 
 int add(int a, int b){
 	return a+b;
@@ -27,7 +28,7 @@ int (*choice(int a))(int, int){
 		return mul;
 	}
 }
-
+//choice1 is function taking character as an argument and returning a function pointer which takes an integer argument which returns a function pointer which takes two integers arguments and returns an integer. 
 int (*(*choice1(char a))(int))(int, int){
 	if(a=='a'){
 		return choice;
@@ -36,9 +37,19 @@ int (*(*choice1(char a))(int))(int, int){
 	}
 }
 
+//choice3 is a function which take no arguments and returns a function pointer which takes character as an argument and returning a function pointer which takes an integer argument which returns a function pointer which takes two integers arguments and returns an integer.
+int (*(*(*choice3())(char))(int))(int, int){
+	return choice1;	
+}
+
 int main(void){
+	choice4 *f5 = choice3();
+	choice2 *f6 = f5('a');
+	//int *((*choicex(int a))(int, int)) = NULL; How to use it with out TypeDef?
+	fptr fptr2 = f6(CHOICE);
+	printf("%d\n", fptr2(10, 20));
 	choice2 *f2 = choice1('a');
-	fptr fptr1 = f2(1);
+	fptr fptr1 = f2(CHOICE);
 	printf("%d\n", fptr1(10, 20));
 	fptr f1 = choice(CHOICE);
 	printf("%d\n", f1(10, 20));	
